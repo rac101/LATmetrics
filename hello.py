e8f20062873ce2b14644c87d3c36c4760b1fdcbe
Robert Cameron <rac@stanford.edu>
2015-09-29 15:32:10 -0700

    initial commit of the LATmetrics python notebooks

---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 01:53:51 2015
+
+@author: rc
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None"""
+    print("Hello World")
+
+# main program starts here
+hello()
